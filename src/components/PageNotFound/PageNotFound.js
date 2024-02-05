5364acda43abd09fcbe1bc70a778ebb5ebaafdd2
Karan-Tatva24 <karan.sarvaiya@etatvasoft.com>
2024-02-05 18:06:57 +0530

    Add Page not found route and component

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,10 @@
 import React from "react";
 import pnf from "../../images/pnf.png";
+import './PageNotFound.css'
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className="pnf">
       <img src={pnf} alt="Page Not Found" />
     </div>
   );
